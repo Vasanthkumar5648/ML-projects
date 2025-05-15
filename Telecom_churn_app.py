bc5e2b50e85b1fa6f13121998312f5f37f46260a
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 00:39:00 +0530

    Update Telecom_churn_app.py

--- Telecom_churn_app.py
+++ Telecom_churn_app.py
@@ -7,4 +7,5 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.metrics import accuracy_score, classification_report
 
+https://github.com/Vasanthkumar5648/ML-projects/blob/main/Telco_Customer_Churn%20(1).csv
 
