e37b0a64cd37f748b745e2b7312ee7fa58bce95c
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-15 16:32:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
   
