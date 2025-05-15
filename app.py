f00c5d36c96fb6cd8cf22323dca32aef78690ea2
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-15 22:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,13 @@ with st.sidebar:
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
-  
+
+  with st.expander('Input features'):
+  st.write('**Input penguins**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
+
   encode = ['island','sex']
   df_penguins = pd.get_dummies(input_penguins, prefix=encode)
   input_row=df_penguins[:1]
@@ -52,11 +58,9 @@ with st.sidebar:
   y = y_raw.apply(target_encode)
   y
   y_raw
-  
-with st.expander('Input features'):
-  st.write('**Input penguins**')
-  input_df
-  st.write('**Combined penguins data**')
-  input_penguins
-  st.write('**Encoded Input penguin**')
+
+with st.expander('Data preparation'):  
+  st.write('**Encoded X (input penguin)**')
   input_row
+  st.write('**Encoded y**')
+  y
