094d9d35d8f90e43c55f8752fc5ab4c1d13cc661
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 00:50:52 +0530

    Update Telecom_churn_app.py

--- Telecom_churn_app.py
+++ Telecom_churn_app.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.metrics import accuracy_score, classification_report
 
-data = pd.read_csv('https://github.com/Vasanthkumar5648/ML-projects/blob/main/Telco_Customer_Churn%20(1).csv')
+data = pd.read_csv('https://raw.githubusercontent.com/Vasanthkumar5648/ML-projects/main/Telco_Customer_Churn%20(1).csv')
 data.drop(columns=['customerID'], inplace=True) 
 
 le = LabelEncoder() 
