cf50ce31d810b877e35584b40ca51e7a70f8707a
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-15 22:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-
 
 st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
+
 with st.expander('Data'):
-  st.write('**Raw Data**')
+  st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
@@ -20,8 +21,9 @@ with st.expander('Data'):
   y_raw
 
 with st.expander('Data visualization'):
-                 st.scatter_chart(data=df,x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
+# Input features
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
@@ -42,35 +44,41 @@ with st.sidebar:
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input features'):
-  st.write('**Input penguins**')
+  st.write('**Input penguin**')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
 
-  encode = ['island','sex']
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  
-  X = df_penguins[1:]
-  input_row=df_penguins[:1]
-  
-  target_mapper = {'Adelie': 0,
-                   'Chinstrap': 1,
-                   'Gentoo':2}
-  def target_encode(val):
-    return target_mapper[val]
-    
-  y = y_raw.apply(target_encode)
-  y
-  y_raw
 
-with st.expander('Data preparation'):  
+# Data preparation
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
   st.write('**Encoded X (input penguin)**')
   input_row
   st.write('**Encoded y**')
   y
 
+
+# Model training and inference
+## Train the ML model
 clf = RandomForestClassifier()
 clf.fit(X, y)
 
+## Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
