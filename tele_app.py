994bcdd5b3bc3026d02229dde280eaa1a67c095e
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 01:12:44 +0530

    Update tele_app.py

--- tele_app.py
+++ tele_app.py
@@ -40,9 +40,55 @@ if model is None or scaler is None:
 
 # Rest of your Streamlit app code...
 def user_input_features():
-    # Collect user inputs (same as before)
-    # ... [rest of your input collection code]
-
+    # Collect user inputs
+    gender = st.sidebar.selectbox('Gender', ['Male', 'Female'])
+    SeniorCitizen = st.sidebar.selectbox('Senior Citizen', ['No', 'Yes'])
+    Partner = st.sidebar.selectbox('Partner', ['No', 'Yes'])
+    Dependents = st.sidebar.selectbox('Dependents', ['No', 'Yes'])
+    tenure = st.sidebar.slider('Tenure (months)', 1, 72, 12)
+    PhoneService = st.sidebar.selectbox('Phone Service', ['No', 'Yes'])
+    MultipleLines = st.sidebar.selectbox('Multiple Lines', ['No', 'Yes', 'No phone service'])
+    InternetService = st.sidebar.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
+    OnlineSecurity = st.sidebar.selectbox('Online Security', ['No', 'Yes', 'No internet service'])
+    OnlineBackup = st.sidebar.selectbox('Online Backup', ['No', 'Yes', 'No internet service'])
+    DeviceProtection = st.sidebar.selectbox('Device Protection', ['No', 'Yes', 'No internet service'])
+    TechSupport = st.sidebar.selectbox('Tech Support', ['No', 'Yes', 'No internet service'])
+    StreamingTV = st.sidebar.selectbox('Streaming TV', ['No', 'Yes', 'No internet service'])
+    StreamingMovies = st.sidebar.selectbox('Streaming Movies', ['No', 'Yes', 'No internet service'])
+    Contract = st.sidebar.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+    PaperlessBilling = st.sidebar.selectbox('Paperless Billing', ['No', 'Yes'])
+    PaymentMethod = st.sidebar.selectbox('Payment Method', 
+                                       ['Electronic check', 'Mailed check', 
+                                        'Bank transfer (automatic)', 'Credit card (automatic)'])
+    MonthlyCharges = st.sidebar.slider('Monthly Charges ($)', 18.0, 120.0, 50.0)
+    TotalCharges = st.sidebar.slider('Total Charges ($)', 18.0, 9000.0, 1000.0)
+    
+    # Create a dictionary of the inputs
+    data = {
+        'gender': gender,
+        'SeniorCitizen': 1 if SeniorCitizen == 'Yes' else 0,
+        'Partner': Partner,
+        'Dependents': Dependents,
+        'tenure': tenure,
+        'PhoneService': PhoneService,
+        'MultipleLines': MultipleLines,
+        'InternetService': InternetService,
+        'OnlineSecurity': OnlineSecurity,
+        'OnlineBackup': OnlineBackup,
+        'DeviceProtection': DeviceProtection,
+        'TechSupport': TechSupport,
+        'StreamingTV': StreamingTV,
+        'StreamingMovies': StreamingMovies,
+        'Contract': Contract,
+        'PaperlessBilling': PaperlessBilling,
+        'PaymentMethod': PaymentMethod,
+        'MonthlyCharges': MonthlyCharges,
+        'TotalCharges': TotalCharges
+    }
+    
+    # Convert to DataFrame
+    features = pd.DataFrame(data, index=[0])
+    return features
 input_df = user_input_features()
 
 # Display and prediction code
