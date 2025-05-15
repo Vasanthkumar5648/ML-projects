1069307735d229530974669119e56a64a6b9b985
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-15 22:41:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,12 +73,3 @@ with st.expander('Data preparation'):
   st.write('**Encoded y**')
   y
 
-
-# Model training and inference
-## Train the ML model
-clf = RandomForestClassifier()
-clf.fit(X, y)
-
-## Apply model to make predictions
-prediction = clf.predict(input_row)
-prediction_proba = clf.predict_proba(input_row)
