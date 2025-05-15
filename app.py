b33aa77a322d8738dee5dc88c65da74267b77732
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-15 18:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,11 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('male','female'))
                     
   data = {'island',island,
-          'bill_length_mm':bill_length_mm,
-          'bill_depth_mm':bill_depth_mm,
-          'fliper_length_mm':fliper_length_mm,
-          'body_mass_g':body_mass_g,
-          'gender':gender}
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'fliper_length_mm': fliper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 input_df
