819211e3dde5980d56c5815a1299958601a85133
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 00:36:54 +0530

    Create Telecom_churn_app.py

---
+++ Telecom_churn_app.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler, LabelEncoder 
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.metrics import accuracy_score, classification_report
+
+
