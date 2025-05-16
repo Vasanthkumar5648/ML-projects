38776790c96583866f79649f235d2fbfa4ed23a7
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-16 19:15:15 +0530

    Update Telecom_Customer_Churn_app.py

--- Telecom_Customer_Churn/Telecom_Customer_Churn_app.py
+++ Telecom_Customer_Churn/Telecom_Customer_Churn_app.py
@@ -79,7 +79,7 @@ total_charges = st.sidebar.selectbox("Total Charges", list(range(0, 10100, 10)))
 # Predict Button
 if st.sidebar.button("Predict Churn"):
     # Display selected input values with bold black text
-    st.subheader("🔍 Selected Input Values")
+    st.subheader("<p style='font-weight:bold; color:black;'>🔍 Selected Input Values:</p>", unsafe_allow_html=True")
     col1, col2 = st.columns(2)
     
     with col1:
