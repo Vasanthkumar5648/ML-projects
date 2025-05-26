77dcd7ca3b364ea283e230cd8505b36a6afb7016
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-26 11:45:18 +0530

    Update breast_cancer_prediction.py

--- Breast Cancer/breast_cancer_prediction.py
+++ Breast Cancer/breast_cancer_prediction.py
@@ -1,19 +1,4 @@
-import streamlit as st
-#import pandas as pd
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler
-#from sklearn.linear_model import LogisticRegression
-#from sklearn.metrics import (accuracy_score, confusion_matrix, 
-                            classification_report, roc_auc_score, roc_curve)
-#import pickle
 
-# Set page config
-st.set_page_config(page_title="Breast Cancer Classifier", layout="wide")
-
-# Title
 st.title("Breast Cancer Classification App")
 st.write("""
 This app uses a Logistic Regression model to classify breast cancer tumors as malignant or benign.
